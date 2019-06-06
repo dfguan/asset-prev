a28a33c65af2b82a23b0a261e10c07fcda610ce2
dfguan <dfguan@hit.edu.cn>
2019-06-06 08:37:44 +0100

    add tehnology distribution

--- scripts/ast_stat.py
+++ scripts/ast_stat.py
@@ -2,6 +2,17 @@
 # Calculate absolute and relative assembly score
 # N50, maximum, total, Ns
 import sys
+
+# colored print  borrow from https://python-forum.io/Thread-Colored-text-output-in-python-3
+black = lambda text: '\033[0;30m' + text + '\033[0m'
+red = lambda text: '\033[0;31m' + text + '\033[0m'
+green = lambda text: '\033[0;32m' + text + '\033[0m'
+yellow = lambda text: '\033[0;33m' + text + '\033[0m'
+blue = lambda text: '\033[0;34m' + text + '\033[0m'
+magenta = lambda text: '\033[0;35m' + text + '\033[0m'
+cyan = lambda text: '\033[0;36m' + text + '\033[0m'
+white = lambda text: '\033[0;37m' + text + '\033[0m'
+
 def col_evd(min_suprt, evd_fn, ctg_evd):
     tsc = 0
     tlen = 0
@@ -9,6 +20,7 @@ def col_evd(min_suprt, evd_fn, ctg_evd):
     prere = -1
     prectg = ""
     ctge = -1
+    suprt_tech_distri = [0,0,0,0,0] # need to change if applied techs are changed
     with open(evd_fn) as f:
         for ln in f:
             lnlist = ln.strip().split('\t')
@@ -41,6 +53,7 @@ def col_evd(min_suprt, evd_fn, ctg_evd):
                 prere = -1
 
             tsc += (re - rs + 1) * ntech
+            suprt_tech_distri[ntech] += (re  - rs + 1)
             ctge = re
         if prers != -1:
             ctg_evd[prectg].append([prers, prere])
@@ -48,7 +61,7 @@ def col_evd(min_suprt, evd_fn, ctg_evd):
             tlen += ctge
         f.close()
                                                      
-    return [tsc, tlen]
+    return [tsc, tlen, suprt_tech_distri]
 
 def stat(ctg_evd):
 		# new list of value   
@@ -71,9 +84,14 @@ def stat(ctg_evd):
     v = [ls[e] for e in idx]
     return (suma, maxa, lls, idx, v)
 
-def print_stat(absco, suma, lls, maxa, idx, v, ntech):
-    print ("absolute assembly score: {0:.2f}".format(absco))
-    print ("relative assembly score: {0:.2f}".format(absco/ntech)) 
+def print_stat(tscore, suptd, tlen, suma, lls, maxa, idx, v, ntech):
+    print (green("assembly scores:"))
+    print ("absolute assembly score: {0:.2f}".format(tscore/tlen))
+    print ("relative assembly score: {0:.2f}".format(tscore/tlen/ntech))
+    print ("")
+    print (green("support technology distribution (%) [0-{0} techs]:".format(ntech)) + " {1}".format(ntech, ' '.join(["{0:.2f}".format(x/tlen * 100) for x in suptd])))
+    print ("")
+    print (green("reliable blocks statistics [>=2 techs]")) 
     print ("sum: {0}, largest: {1}, average: {2:.2f}".format(suma, maxa, suma/lls)) 
     # print (v)
     for z in [50, 60, 70, 80, 90, 100]:
@@ -86,8 +104,8 @@ if __name__ == "__main__":
     min_suprt = 2
     acc_fn = sys.argv[1]
     ctg_evd = {}
-    [tscore, tlen] =col_evd(min_suprt, acc_fn, ctg_evd)
+    [tscore, tlen, suptd] =col_evd(min_suprt, acc_fn, ctg_evd)
     (suma, maxa, lls, idx, v) = stat(ctg_evd) 
-    print_stat(tscore/tlen, suma, lls, maxa, idx, v, 4)    
+    print_stat(tscore, suptd, tlen, suma, lls, maxa, idx, v, 4)    
 
 
